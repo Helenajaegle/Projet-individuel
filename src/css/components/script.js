cf0d59fa594cb008d1acf0d40cf92b4007e75857
Helenajaegle <94958481+Helenajaegle@users.noreply.github.com>
2022-01-07 20:57:47 +0100

    commit 4

--- src/css/components/script.js
+++ src/css/components/script.js
@@ -16,16 +16,18 @@ function carrouselprojetindi() {
         targets: images155,
         translateX: [{value: '375px', duration: d},
                      {value: '-375px', duration: 0, delay: function(img,ind) { 
-                        if (ind == 0) return (5*delta)+(4*d);
-                        if (ind == 1) return (4*delta)+(3*d);
-                        if (ind == 2) return (3*delta)+(2*d);
-                        if (ind == 3) return (2*delta)+(1*d);
-                        if (ind == 4) return (1*delta)+d;
-                        if (ind == 5) return delta;
+                        if (ind == 0) return (8*delta)+(7*d);
+                        if (ind == 1) return (7*delta)+(6*d);
+                        if (ind == 2) return (6*delta)+(5*d);
+                        if (ind == 3) return (5*delta)+(4*d);
+                        if (ind == 4) return (4*delta)+(3*d);
+                        if (ind == 5) return (3*delta)+(2*d);
+                        if (ind == 6) return (2*delta)+d;
+                        if (ind == 7) return delta;
                         return 0;
                      }},
                      {value: '0', duration: function(img,ind) {
-                         if (ind == 4) return 0;
+                         if (ind == 6) return 0;
                          return d;
                      }}],
         easing: 'linear',
@@ -36,6 +38,8 @@ function carrouselprojetindi() {
             if (ind == 3) return (4*delta)+(3*d);
             if (ind == 4) return (5*delta)+(4*d);
             if (ind == 5) return (6*delta)+(5*d);
+            if (ind == 6) return (7*delta)+(6*d);
+            if (ind == 7) return (8*delta)+(7*d);
             return (6*delta)+(5*d);
         }
     });

0988b7de6cf3856455c8f7dce64dd07784239d75
Helenajaegle <94958481+Helenajaegle@users.noreply.github.com>
2022-01-08 14:38:35 +0100

    commit 6

--- src/css/components/script.js
+++ src/css/components/script.js
@@ -44,3 +44,44 @@ function carrouselprojetindi() {
         }
     });
 }
+
+function carrouselfrise() {
+    let images155 = document.querySelectorAll('.image155');
+    let d = 2000;
+    let delta = 1000;
+
+    images155.forEach(function(img,indice) {
+        img.style.zIndex = - indice;
+    });
+
+    let anim155 = anime.timeline({
+        loop: true,
+        delay: 0
+    });
+
+    anim155.add({
+        targets: images155,
+        translateX: [{value: '375px', duration: d},
+                     {value: '-375px', duration: 0, delay: function(img,ind) { 
+                        if (ind == 0) return (8*delta)+(4*d);
+                        if (ind == 1) return (7*delta)+(3*d);
+                        if (ind == 2) return (6*delta)+(2*d);
+                        if (ind == 6) return (2*delta)+d;
+                        if (ind == 7) return delta;
+                        return 0;
+                     }},
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 3) return 0;
+                         return d;
+                     }}],
+        easing: 'linear',
+        delay: function(img,ind) {
+            if (ind == 0) return delta;
+            if (ind == 1) return (2*delta)+d;
+            if (ind == 2) return (3*delta)+(2*d);
+            if (ind == 3) return (4*delta)+(3*d);
+            if (ind == 4) return (5*delta)+(4*d);
+            return (3*delta)+(2*d);
+        }
+    });
+}
